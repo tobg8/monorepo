32ae88110bc5ea5c1c0f7961a242e44bc36c8d97
theo <theo.beloeil.pro@gmail.com>
2024-05-27 14:34:04 +0200

    init monorepo with bazel

---
+++ domains/sample_app/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	_ "embed"
+
+	"github.com/gorilla/mux"
+
+	sl "github.com/monorepo/domains/sample_library"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	log.Print(r)
+	_, err := w.Write([]byte(sl.SampleGoFunction("Akhilesh")))
+	if err != nil {
+		return
+	}
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+
+	return port
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	// Bind to a port and pass our router in
+	port := getPort()
+	log.Println("Going to listen on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+
+}
